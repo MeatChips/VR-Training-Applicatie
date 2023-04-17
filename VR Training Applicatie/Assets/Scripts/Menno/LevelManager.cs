f20f351ecf7099025bdab651d77ef748afacbe99
Menno <114731270+Buldz@users.noreply.github.com>
2023-04-17 15:26:12 +0200

    Audio toegevoegt

--- VR Training Applicatie/Assets/Scripts/Menno/LevelManager.cs
+++ VR Training Applicatie/Assets/Scripts/Menno/LevelManager.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
     public string SceneName;
+
+    public AudioSource NormalButtonClick;
+    public AudioSource PlayButtonClick;
  
 
     public void LoadScene()
@@ -18,5 +22,15 @@ public class LevelManager : MonoBehaviour
     {
         Application.Quit();
         Console.WriteLine("BYE!!!");
+    }
+
+    public void PlayButtonAudio()
+    {
+        PlayButtonClick.Play();
+    }
+
+    public void NormalButtonAudio()
+    {
+        NormalButtonClick.Play();
     }
 }
